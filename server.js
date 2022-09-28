08cc53028cb0fc25538e8c00c9ad2262bc96359e
Nawab Ali <abdulmuqsit10101@gmail.com>
2022-09-28 17:40:09 +0500

    fix: created layouts and connection established ith mongoose

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -6,11 +10,19 @@ const router = require('./routes');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.set('layouts', 'layouts/layout');
+app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.error('Connected to Mongoose'));
 
 app.use('/', router);
 
 app.listen(process.env.PORT || 3000);
-
